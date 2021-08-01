a5a3f65b1c9d29b6342ac3076ac48cda7b3d763d
chris-fullinwider <chris.fullinwider@gmail.com>
2021-07-31 18:18:24 -0600

    add in form switching functionality > add form state management

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Box, Button, TextField } from '@material-ui/core';
 import logo from '../brand_logo.svg';
@@ -10,23 +10,29 @@ interface ILoginFormProps {
 
 const RegistrationForm: React.FunctionComponent<ILoginFormProps> = (props: ILoginFormProps) => {
   const { setIsLoginForm } = props
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <form>
       <img src={logo} alt="this is the brand, baby" />
       <Box display="flex" flexDirection="column">
         <TextField
+          value={email}
           className="form-input"
           id="registration-email"
           label="Email"
           variant="outlined"
           color="primary"
+          onChange={(evt) => setEmail(evt.target.value)}
         />
         <TextField
+          value={password}
           className="form-input"
           id="registration-password"
           label="Password"
           variant="outlined"
           color="primary"
+          onChange={(evt) => setPassword(evt.target.value)}
         />
         <Button
           className="form-button"
--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,7 +1,7 @@
 import {
   Box, Button, TextField,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../brand_logo.svg';
 
 interface IRegistrationFormProps {
@@ -10,37 +10,49 @@ interface IRegistrationFormProps {
 
 const RegistrationForm: React.FunctionComponent<IRegistrationFormProps> = (props: IRegistrationFormProps) => {
   const { setIsLoginForm } = props
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   return (
     <form>
       <img src={logo} alt="this is the brand, baby" />
       <Box display="flex" flexDirection="column">
         <TextField
+          value={name}
           className="form-input"
           id="registration-name"
           label="Full Name"
           variant="outlined"
           color="primary"
+          onChange={(evt) => setName(evt.target.value)}
         />
         <TextField
+          value={email}
           className="form-input"
           id="registration-email"
           label="Email"
           variant="outlined"
           color="primary"
+          onChange={(evt) => setEmail(evt.target.value)}
         />
         <TextField
+          value={password}
           className="form-input"
           id="registration-password"
           label="Password"
           variant="outlined"
           color="primary"
+          onChange={(evt) => setPassword(evt.target.value)}
         />
         <TextField
+          value={confirmPassword}
           className="form-input"
           id="registration-password-confirm"
           label="Confirm Password"
           variant="outlined"
           color="primary"
+          onChange={(evt) => setConfirmPassword(evt.target.value)}
         />
         <Button
           className="form-button"
